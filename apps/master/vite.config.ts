f9124e251b595dc1ab7b54811d68d2a4f92a0789
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2025-05-22 06:03:32 +0200

    chore: add initial setup for master application with routing and Cypress E2E tests

---
+++ apps/master/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/master',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [
+    reactRouter(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+  ],
+  build: {
+    outDir: '../../dist/apps/master',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
