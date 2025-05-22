b070440a9b002695b2cd6cd3f5610882d53e4114
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2025-05-22 06:12:14 +0200

    chore: set up agent and e2e applications with TypeScript, ESLint, and Cypress configurations

--- apps/master/react-router.config.ts
+++ apps/master/react-router.config.ts
@@ -1,6 +1,16 @@
 import type { Config } from '@react-router/dev/config';
+import fsExtra from 'fs-extra';
 
 export default {
   ssr: true,
   routeDiscovery: { mode: 'initial' },
+  buildEnd: () => {
+    fsExtra.removeSync('../../dist/apps/master');
+    fsExtra.copySync('build', '../../dist/apps/master/build', {
+      dereference: true,
+    });
+    fsExtra.copySync('package.json', '../../dist/apps/master/package.json', {
+      dereference: true,
+    });
+  },
 } satisfies Config;
